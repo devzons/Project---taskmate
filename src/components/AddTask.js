a336bea24b7410ab87e4f59810ce9aaada747d0c
Don Yu <37088038+devzons@users.noreply.github.com>
2023-03-28 17:07:43 -0700

    Added tasklist and setTasklist

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,20 @@
-export const AddTask = () => {
+export const AddTask = ({ tasklist, setTasklist }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const date = new Date()
+    const newTask = {
+      id: date.getTime(),
+      name: e.target.task.value,
+      time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+    }
+    setTasklist([...tasklist, newTask])
+    e.target.task.value = ''
+  }
+
   return (
     <section className='addTask'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type='text' name='task' id='task' autocomplete='off' maxLength='25' placeholer='add task' />
         <button type='submit'>Add</button>
       </form>
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,23 +1,19 @@
-export const ShowTask = () => {
-  const tasks = [
-    { id: 10001, name: 'TASK A', time: '2:08:01 AM 3/28/2023' },
-    { id: 10002, name: 'TASK B', time: '2:08:01 AM 3/28/2023' },
-    { id: 10003, name: 'TASK C', time: '2:08:01 AM 3/28/2023' },
-  ]
-
+export const ShowTask = ({ tasklist, setTasklist }) => {
   return (
     <section className='showTask'>
       <div className='head'>
         <div>
           <span className='title'>Todo</span>
-          <span className='count'>{tasks.length}</span>
+          <span className='count'>{tasklist.length}</span>
         </div>
-        <button className='clearAll'>Clear All</button>
+        <button className='clearAll' onClick={() => setTasklist([])}>
+          Clear All
+        </button>
       </div>
       <ul>
-        {tasks &&
-          tasks.map((task) => (
-            <li>
+        {tasklist &&
+          tasklist.map((task) => (
+            <li key={task.id}>
               <p>
                 <span className='name'>{task.name}</span>
                 <span className='time'>{task.time}</span>
